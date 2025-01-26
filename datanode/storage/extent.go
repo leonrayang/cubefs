848c25e38cd3c70dc863135f5382cf37a4f76515
leonrayang <chl696@sina.com>
2025-01-26 17:09:27 +0800

    feat(datanode): Optimize the memory allocation in log generating
    Signed-off-by: leonrayang <chl696@sina.com>

--- datanode/storage/extent.go
+++ datanode/storage/extent.go
@@ -17,12 +17,12 @@ package storage
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"hash/crc32"
 	"io"
 	"math"
 	"os"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -615,7 +615,9 @@ func (e *Extent) autoComputeExtentCrc(extSize int64, crcFunc UpdateCrcFunc) (crc
 
 // DeleteTiny deletes a tiny extent.
 func (e *Extent) punchDelete(offset, size int64) (hasDelete bool, err error) {
-	log.LogDebugf("punchDelete extent %v offset %v, size %v", e, offset, size)
+	if log.EnableDebug() {
+		log.LogDebugf("punchDelete extent %v offset %v, size %v", e, offset, size)
+	}
 	if int(offset)%util.PageSize != 0 {
 		return false, ParameterMismatchError
 	}
@@ -625,7 +627,7 @@ func (e *Extent) punchDelete(offset, size int64) (hasDelete bool, err error) {
 
 	newOffset, err := e.file.Seek(offset, SEEK_DATA)
 	if err != nil {
-		if strings.Contains(err.Error(), syscall.ENXIO.Error()) {
+		if errors.Is(err, syscall.ENXIO) {
 			return true, nil
 		}
 		return false, err
@@ -633,7 +635,9 @@ func (e *Extent) punchDelete(offset, size int64) (hasDelete bool, err error) {
 	if newOffset-offset >= size {
 		return true, nil
 	}
-	log.LogDebugf("punchDelete offset %v size %v", offset, size)
+	if log.EnableDebug() {
+		log.LogDebugf("punchDelete offset %v size %v", offset, size)
+	}
 	err = fallocate(int(e.file.Fd()), util.FallocFLPunchHole|util.FallocFLKeepSize, offset, size)
 	return
 }
